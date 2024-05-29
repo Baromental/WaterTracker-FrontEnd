7e6514ecbe5aac7899905ad8068f50370fc1a8bd
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-05-29 18:25:38 +0300

    auth redux

---
+++ src/redux/store.js
@@ -0,0 +1,37 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
+import { authReducer } from './auth/authSlice';
+
+const persistConfig = {
+  key: 'auth',
+  version: 1,
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(persistConfig, authReducer),
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+export const persistor = persistStore(store);
