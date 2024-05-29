01391ce94806acf28026c118ada30cb7c662c663
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-05-29 15:31:29 +0300

    add AuthForm
    modern-normalize
    react-hook-form
    react-router-dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from './Header/Header';
 
+import Header from './Header/Header';
+import { AuthForm } from './AuthForm/AuthForm';
+
+import 'modern-normalize';
 
 const App = () => {
   return (
-    <div >
+    <div>
       <Routes>
-        <Route path="/" element={<Header/>}></Route>
-      </Routes> 
+        <Route path="/signup" element={<AuthForm formType={'register'} />} />
+        <Route path="/signin" element={<AuthForm formType={'login'} />} />
+        <Route path="/" element={<Header />}></Route>
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
