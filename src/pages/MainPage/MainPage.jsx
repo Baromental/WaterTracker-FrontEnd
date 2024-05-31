102c0393991afe50fd84c753a9280d21df5ff6f2
Nataliia Plokhotnyk <shmilo.nataliya@gmail.com>
2024-05-31 13:41:00 +0300

    BgSectionHome

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
+import { BgSectionHome } from '../../components/BgSectionHome/BgSectionHome';
 
 const MainPage = () => {
-  return <TodayWaterList />;
+  return (
+    <BgSectionHome>
+      <TodayWaterList />
+    </BgSectionHome>
+  );
 };
 
 export default MainPage;
