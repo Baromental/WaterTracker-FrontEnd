6032d715fd18f24d36d5950621511776ff24b844
Maksym <suzdalcevmaksym@gmail.com>
2024-06-10 21:08:42 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,9 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/watertracker-frontend/',
     build: {
       sourcemap: true,
-
       rollupOptions: {
         input: glob.sync('./src/*.html'),
         output: {
